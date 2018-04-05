1eaed3f9f6dbf2784e0d57466d5bff60805605ea
Omar Zaragoza <zaragoza.sosa@gmail.com>
2018-04-05 02:57:59 -0700

    Simon's quest demo

--- www/bin/app.js
+++ www/bin/app.js
@@ -1,15 +1,166 @@
-var SimpleGame = (function () {
-    function SimpleGame() {
-        this.game = new Phaser.Game(800, 600, Phaser.AUTO, 'content', { create: this.create });
-    }
-    SimpleGame.prototype.create = function () {
-        var text = "Hello World!";
-        var style = { font: "65px Arial", fill: "#ff0000", align: "center" };
-        this.game.add.text(0, 0, text, style);
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
-    return SimpleGame;
-}());
-window.onload = function () {
-    var game = new SimpleGame();
-};
+})();
+var MyGame;
+(function (MyGame) {
+    var Boot = (function (_super) {
+        __extends(Boot, _super);
+        function Boot() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Boot.prototype.init = function () {
+            this.input.maxPointers = 1;
+            this.stage.disableVisibilityChange = true;
+            this.game.physics.startSystem(Phaser.Physics.ARCADE);
+            if (this.game.device.desktop) {
+                this.scale.pageAlignHorizontally = true;
+            }
+            else {
+                this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+                this.scale.setMinMax(480, 260, 1024, 768);
+                this.scale.forceLandscape = true;
+                this.scale.pageAlignHorizontally = true;
+            }
+        };
+        Boot.prototype.preload = function () {
+            this.load.image('preloadBar', 'img/loader.png');
+        };
+        Boot.prototype.create = function () {
+            this.game.state.start('Preloader');
+        };
+        return Boot;
+    }(Phaser.State));
+    MyGame.Boot = Boot;
+})(MyGame || (MyGame = {}));
+var MyGame;
+(function (MyGame) {
+    var Game = (function (_super) {
+        __extends(Game, _super);
+        function Game() {
+            var _this = _super.call(this, 800, 600, Phaser.AUTO, 'content', null) || this;
+            _this.state.add('Boot', MyGame.Boot, false);
+            _this.state.add('Preloader', MyGame.Preloader, false);
+            _this.state.add('MainMenu', MyGame.MainMenu, false);
+            _this.state.add('Level1', MyGame.Level1, false);
+            _this.state.start('Boot');
+            return _this;
+        }
+        return Game;
+    }(Phaser.Game));
+    MyGame.Game = Game;
+})(MyGame || (MyGame = {}));
+var MyGame;
+(function (MyGame) {
+    var Level1 = (function (_super) {
+        __extends(Level1, _super);
+        function Level1() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        Level1.prototype.create = function () {
+            this.background = this.add.sprite(0, 0, 'level1');
+            this.player = new MyGame.Player(this.game, 130, 284);
+        };
+        return Level1;
+    }(Phaser.State));
+    MyGame.Level1 = Level1;
+})(MyGame || (MyGame = {}));
+var MyGame;
+(function (MyGame) {
+    var MainMenu = (function (_super) {
+        __extends(MainMenu, _super);
+        function MainMenu() {
+            return _super !== null && _super.apply(this, arguments) || this;
+        }
+        MainMenu.prototype.create = function () {
+            this.music = this.add.audio('titleMusic');
+            this.music.play();
+            this.background = this.add.sprite(0, 0, 'titlepage');
+            this.background.alpha = 0;
+            this.logo = this.add.sprite(this.world.centerX, -300, 'logo');
+            this.logo.anchor.setTo(0.5, 0.5);
+            this.add.tween(this.background).to({ alpha: 1 }, 2000, Phaser.Easing.Bounce.InOut, true);
+            this.add.tween(this.logo).to({ y: 220 }, 2000, Phaser.Easing.Elastic.Out, true, 2000);
+            this.input.onDown.addOnce(this.fadeOut, this);
+        };
+        MainMenu.prototype.fadeOut = function () {
+            this.add.tween(this.background).to({ alpha: 0 }, 2000, Phaser.Easing.Linear.None, true);
+            var tween = this.add.tween(this.logo).to({ y: 800 }, 2000, Phaser.Easing.Linear.None, true);
+            tween.onComplete.add(this.startGame, this);
+        };
+        MainMenu.prototype.startGame = function () {
+            this.music.stop();
+            this.game.state.start('Level1', true, false);
+        };
+        return MainMenu;
+    }(Phaser.State));
+    MyGame.MainMenu = MainMenu;
+})(MyGame || (MyGame = {}));
+var MyGame;
+(function (MyGame) {
+    var Player = (function (_super) {
+        __extends(Player, _super);
+        function Player(game, x, y) {
+            var _this = _super.call(this, game, x, y, 'simon', 0) || this;
+            _this.game.physics.arcade.enableBody(_this);
+            _this.anchor.setTo(0.5, 0);
+            _this.animations.add('walk', [0, 1, 2, 3, 4], 10, true);
+            game.add.existing(_this);
+            return _this;
+        }
+        Player.prototype.update = function () {
+            this.body.velocity.x = 0;
+            if (this.game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+                this.body.velocity.x = -150;
+                this.animations.play('walk');
+                if (this.scale.x == 1) {
+                    this.scale.x = -1;
+                }
+            }
+            else if (this.game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+                this.body.velocity.x = 150;
+                this.animations.play('walk');
+                if (this.scale.x == -1) {
+                    this.scale.x = 1;
+                }
+            }
+            else {
+                this.animations.frame = 0;
+            }
+        };
+        return Player;
+    }(Phaser.Sprite));
+    MyGame.Player = Player;
+})(MyGame || (MyGame = {}));
+var MyGame;
+(function (MyGame) {
+    var Preloader = (function (_super) {
+        __extends(Preloader, _super);
+        function Preloader() {
+            var _this = _super !== null && _super.apply(this, arguments) || this;
+            _this.ready = false;
+            return _this;
+        }
+        Preloader.prototype.preload = function () {
+            this.preloadBar = this.add.sprite(300, 400, 'preloadBar');
+            this.load.setPreloadSprite(this.preloadBar);
+            this.load.image('titlepage', 'img/titlepage.jpg');
+            this.load.audio('titleMusic', 'audio/title.mp3', true);
+            this.load.image('logo', 'img/logo.png');
+            this.load.spritesheet('simon', 'img/simon.png', 58, 96, 5);
+            this.load.image('level1', 'img/level1.png');
+        };
+        Preloader.prototype.create = function () {
+            this.game.state.start('MainMenu');
+        };
+        return Preloader;
+    }(Phaser.State));
+    MyGame.Preloader = Preloader;
+})(MyGame || (MyGame = {}));
 //# sourceMappingURL=app.js.map
