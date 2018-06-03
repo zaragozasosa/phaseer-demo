a727f8d8089aec5b753d8f836751a0bc0e72a606
Omar Zaragoza <ozaragoza@eastridge.com>
2018-06-03 03:07:53 -0700

    Adding back the compiled js

---
+++ dist/game/Logic/PlayerLogic/PowerGamingLogic.js
@@ -0,0 +1,42 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+var LogicalGrid_1 = require("./../LogicalGrid");
+var PowerGamingLogic = (function (_super) {
+    __extends(PowerGamingLogic, _super);
+    function PowerGamingLogic(gameboardConfig) {
+        return _super.call(this, gameboardConfig) || this;
+    }
+    PowerGamingLogic.prototype.power = function () {
+        var tiles = this.getTilesOrdered();
+        if (this.canUsePower()) {
+            for (var x = 0; x < tiles.length; x++) {
+                if (tiles[x].value < this.gameboardConfig.minimumValue * 32) {
+                    tiles[x].duplicate();
+                }
+            }
+            this.cleanGrid();
+        }
+    };
+    PowerGamingLogic.prototype.canUsePower = function () {
+        var tiles = this.getTilesOrdered();
+        if (tiles.length > 0 &&
+            tiles[tiles.length - 1].value < this.gameboardConfig.minimumValue * 32) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    return PowerGamingLogic;
+}(LogicalGrid_1.default));
+exports.default = PowerGamingLogic;
