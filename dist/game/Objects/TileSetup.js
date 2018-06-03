13427d7512d4c6fc3c98439b92e2aab77749436e
Omar Zaragoza <ozaragoza@eastridge.com>
2018-06-03 02:54:55 -0700

    Clean dist

--- dist/game/Objects/TileSetup.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TileSetup = (function () {
-    function TileSetup() {
-    }
-    return TileSetup;
-}());
-exports.default = TileSetup;
