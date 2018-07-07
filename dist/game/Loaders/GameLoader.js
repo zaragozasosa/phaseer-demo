c2ea3ace0a65622208691ba88e539cc541e2e04a
Omar Zaragoza <zaragoza.sosa@gmail.com>
2018-07-06 19:41:39 -0700

    Assets changes

--- dist/game/Loaders/GameLoader.js
+++ dist/game/Loaders/GameLoader.js
@@ -7,12 +7,8 @@ var CharacterSelectionLoader = (function () {
         loader.image('bullet', 'assets/images/bullet.png');
         loader.image('dice', 'assets/images/dice.png');
         loader.image('diamond', 'assets/images/diamond.png');
-        loader.image('volume-0', 'assets/images/volume-0.png');
-        loader.image('volume-1', 'assets/images/volume-1.png');
-        loader.image('volume-2', 'assets/images/volume-2.png');
         loader.image('witch', 'assets/images/witch.jpeg');
         loader.image('menu', 'assets/images/menu.png');
-        loader.image('blue', 'assets/images/blue.png');
         loader.image('bug', 'assets/images/bug.png');
         loader.image('bullet', 'assets/images/bullet.png');
         loader.image('dice', 'assets/images/dice.png');
